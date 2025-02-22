e82dc36e55ae65c296b820458670b5b5d8376f52
shagid76 <igrish76@gmail.com>
2025-02-21 22:16:00 -0500

    Optimize SecurityConfig

--- src/main/java/us/yarik/CloudFileStorage/configuration/SecurityConfig.java
+++ src/main/java/us/yarik/CloudFileStorage/configuration/SecurityConfig.java
@@ -3,58 +3,51 @@ package us.yarik.CloudFileStorage.configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
-import us.yarik.CloudFileStorage.security.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 @EnableJdbcHttpSession
 @RequiredArgsConstructor
 public class SecurityConfig {
-    private final CustomUserDetailsService userDetailService;
+    public static final int T_30_DAYS_IN_SECONDS = 2592000;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/registration", "/static/**", "/all")
-                        .permitAll()
-                        .anyRequest().authenticated()
-                )
-                .csrf(csrf -> csrf.disable())
-                .formLogin(formLogin -> formLogin
-                        .loginPage("/login")
-                        .usernameParameter("email")
-                        .defaultSuccessUrl("/redirect_to_directory", true)
-                        .permitAll())
-                .rememberMe(me -> me
-                        .rememberMeParameter("rememberMe")
-                        .tokenValiditySeconds(120000))
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/login").permitAll();
+                    auth.requestMatchers("/registration").permitAll();
+                    auth.requestMatchers("/static/**").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(formLogin -> {
+                    formLogin.loginPage("/login");
+                    formLogin.usernameParameter("email");
+                    formLogin.defaultSuccessUrl("/directory");
+                    formLogin.permitAll();
+                })
+                .rememberMe(me -> {
+                    me.tokenValiditySeconds(T_30_DAYS_IN_SECONDS);
+                })
 
-                .logout(log -> log
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login")
-                        .clearAuthentication(true)
-                        .invalidateHttpSession(true)
-                        .permitAll())
-                .sessionManagement(session -> session
-                        .maximumSessions(1)
-                        .expiredUrl("/login"))
+                .logout(log -> {
+                    log.logoutUrl("/logout").permitAll();
+                    log.logoutSuccessUrl("/login").permitAll();
+                    log.clearAuthentication(true);
+                    log.invalidateHttpSession(true);
+                })
+                .sessionManagement(session -> {
+                    session.maximumSessions(1).expiredUrl("/login");
+                })
                 .build();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder auth = http.getSharedObject(AuthenticationManagerBuilder.class);
-        auth.userDetailsService(userDetailService).passwordEncoder(passwordEncoder());
-        return auth.build();
     }
 
     @Bean
