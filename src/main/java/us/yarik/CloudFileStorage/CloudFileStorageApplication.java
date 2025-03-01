17b6309b4294403288cfb83d86c4151a88d54804
shagid76 <igrish76@gmail.com>
2025-02-28 23:56:11 -0500

    i18n

--- src/main/java/us/yarik/CloudFileStorage/CloudFileStorageApplication.java
+++ src/main/java/us/yarik/CloudFileStorage/CloudFileStorageApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CloudFileStorageApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CloudFileStorageApplication.class, args);
 	}
--- src/main/java/us/yarik/CloudFileStorage/configuration/I18nConfig.java
+++ src/main/java/us/yarik/CloudFileStorage/configuration/I18nConfig.java
@@ -1,10 +1,46 @@
 package us.yarik.CloudFileStorage.configuration;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.Locale;
 
 @Configuration
 public class I18nConfig {
-     public static final String DEFAULT_LOCALE = "en";
-     public static final String LOCALE_COOKIE_NAME = "lang";
-     public static final String LOCALE_CHANGE_PARAM = "lang";
+    public static final Locale DEFAULT_LOCALE = Locale.of("en");
+    public static final String LOCALE_COOKIE_NAME = "lang";
+    public static final String LOCALE_CHANGE_PARAM = "lang";
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("i18n/messages");
+        messageSource.setDefaultEncoding(StandardCharsets.UTF_8.displayName());
+        messageSource.setDefaultLocale(DEFAULT_LOCALE);
+        messageSource.setUseCodeAsDefaultMessage(true);
+        messageSource.setFallbackToSystemLocale(false);
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver(LOCALE_COOKIE_NAME);
+        cookieLocaleResolver.setDefaultLocale(DEFAULT_LOCALE);
+        cookieLocaleResolver.setCookieMaxAge(Duration.ofDays(Integer.MAX_VALUE));
+        return cookieLocaleResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName(LOCALE_CHANGE_PARAM);
+        return localeChangeInterceptor;
+    }
 }
