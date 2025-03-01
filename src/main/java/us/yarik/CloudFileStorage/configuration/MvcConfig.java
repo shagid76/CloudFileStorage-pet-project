9fe18403b945763a671dddcbb53b20ee288da7e7
shagid76 <igrish76@gmail.com>
2025-03-01 01:36:39 -0500

    i18n

--- src/main/java/us/yarik/CloudFileStorage/configuration/MvcConfig.java
+++ src/main/java/us/yarik/CloudFileStorage/configuration/MvcConfig.java
@@ -1,14 +1,25 @@
 package us.yarik.CloudFileStorage.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+    @Autowired
+    private LocaleChangeInterceptor localeChangeInterceptor;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor);
     }
 }
